c49e5dd81799ebd87b84df5487d503226784d768
myusername-dot <55500350+myusername-dot@users.noreply.github.com>
2024-05-17 22:14:03 +0300

    Update NewSet.java
    new features and refactoring

--- src/main/java/com/uvideo/NewSet.java
+++ src/main/java/com/uvideo/NewSet.java
@@ -12,11 +12,12 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.opencv.core.Core.NORM_MINMAX;
 import static org.opencv.core.CvType.CV_8UC1;
@@ -38,77 +39,79 @@ public class NewSet {
      * a character, a word, or a string. The length of each element does not affect the
      * distribution of the gradient, which is distributed evenly: FILL_DEPTH / _filling.length.
      * _false - this will not be created. But you can add the string _false to the name of an
-     * already created file if it appears too often or you don't want it to be processed and
+     * already created file if it appears too often, or you don't want it to be processed and
      * displayed. The main thing is that the change does not affect the sorting order of the
      * directory by file names, if you add a string to the end of the name, this will not happen.
      */
 
     private static String fontPatch = "data_set\\fonts\\MS Gothic.ttf";
-    private static final int size = 14;
+    private static final int sizePixels = 26;
     // the first character must always be a space
-    private static final String spaceAnd_default = " /／＼|｜ﾉ＞l∨∧＜";
-    private static final String _dontMoveX = "-ｰ一ﾆ二ニ＝ヽﾍ丶ゝ=";
-    private static final String _dontSpin = "へｲイ十トΤ>ﾝＯ7Yﾚ^７く<七LvVレｿ∠γメ┴フ(ソう";
-    private static final String _dontMove = "¯_\"\\×〃,.{}';乂ｌｉ!iィﾊ从ｨﾔfcハﾞ┤弋シﾄ〝〟`ｧｒア∈三Ｏrｊjいヾ[八1ァﾑ人∠ゝﾟム" +
-            "ミ斗孑≠ﾘん心》こツ≧厶公ｱｪxｭ少t彡込≫チoX厂芹」「└┘┐小リ〔ﾏ廴示ゞ云NしZz()├〈〉";
-    private static final String[] _filling = {"ﾐ", "*", "+", ":"};
-    private static final String _false = "￣＿｀―弐ー．′‐─、¨´‘’゛（）⌒";
+    private static final String spaceAnd_default = " /／＼|｜lﾉ＞＜∧";
+    private static final String _dontMoveX = "-ｰ一ﾆ二ニ＝ヽﾍ丶ゝ=ノへ";
+    private static final String _dontSpin = "<>十七小心厶うフメラ弋イレツシソに";
+    private static final String _dontMove = "¯_\"\\×.,';∠`∈ｌ1ｉi!ΤtLlfDdUuPpHhJｊCcnｒ^７7oＯRrγYy≠≧ｪxｭ≫┴┘└┐┤├()[]" +
+            "ﾚトｲィｨﾏﾔｧﾊハいァアｿﾝﾑﾘリムんこチスゞヾく廴キ人八厂しミる斗孑公少芹乂回云示三从彡込弐曲";
+    private static final String[] _filling = {"ﾐ", "*", "x", "+", ":", "⋅"};
+    private static final String _false = "￣＿｀―ー．′‐─、¨´‘’゛（）⌒";
 
-    private static void createCharsTxt(LinkedList<Pair<String, String>> symbols, String outPatch) {
+    private static void createCharsTxt(LinkedList<Pair<String, java.util.List<Integer>>> symbols, String outPatch) {
         int count = 0;
         File chars = new File(outPatch + "chars.txt");
         try (PrintWriter writer = new PrintWriter(chars, StandardCharsets.UTF_8)) {
             for (var e : symbols) {
                 count++;
-                for (Character c : e.b.toCharArray())
-                    writer.println(String.format("%03d_%s", count, c));
+                for (Integer cp : e.b)
+                    writer.println(String.format("%03d_%s", count, new String(Character.toChars(cp))));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static float chooseSize(int pixDst) throws IOException, FontFormatException {
+    private static Font chooseSize(Font font, int pixDst) throws FontFormatException {
         BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = img.createGraphics();
         float fontSize = pixDst;
         int pixSize = pixDst + 1, counter = 0;
         while (pixSize != pixDst) {
-            if (pixSize < pixDst) fontSize += .5f;
-            if (pixSize > pixDst) fontSize -= .5f;
-            Font font = Font.createFont(Font.TRUETYPE_FONT,
-                    new File(fontPatch)).deriveFont(fontSize);
+            if (pixSize < pixDst) fontSize += .3f;
+            if (pixSize > pixDst) fontSize -= .3f;
+            font = font.deriveFont(fontSize);
             g2d.setFont(font);
             FontMetrics fm = g2d.getFontMetrics();
             pixSize = fm.getHeight();
             counter++;
-            if (counter == 5) throw new FontFormatException("can't get a font of this size");
+            if (counter == 25) throw new FontFormatException(font.getFontName() + " can't get a font of this size");
         }
-        return fontSize;
+        return font;
     }
 
-    private static LinkedList<Pair<String, String>> buildSTrain() {
-        LinkedList<Pair<String, String>> train = new LinkedList<>();
+    private static LinkedList<Pair<String, java.util.List<Integer>>> buildSTrain() {
+        LinkedList<Pair<String, java.util.List<Integer>>> train = new LinkedList<>();
         // adding basic symbols and flags for them
-        for (Character c : spaceAnd_default.toCharArray())
-            train.add(new Pair<>("", c.toString()));
-        for (Character c : _dontMoveX.toCharArray())
-            train.add(new Pair<>("_dont_move_x", c.toString()));
-        for (Character c : _dontSpin.toCharArray())
-            train.add(new Pair<>("_dont_spin", c.toString()));
-        for (Character c : _dontMove.toCharArray())
-            train.add(new Pair<>("_dont_move", c.toString()));
+        for (int cp : spaceAnd_default.codePoints().toArray())
+            train.add(new Pair<>("", List.of(cp)));
+        for (int cp : _dontMoveX.toCharArray())
+            train.add(new Pair<>("_dont_move_x", List.of(cp)));
+        for (int cp : _dontSpin.toCharArray())
+            train.add(new Pair<>("_dont_spin", List.of(cp)));
+        for (int cp : _dontMove.toCharArray())
+            train.add(new Pair<>("_dont_move", List.of(cp)));
         // adding a gradient
         for (String f : _filling)
-            train.add(new Pair<>("_filling", f));
+            train.add(new Pair<>("_filling", f.codePoints().boxed().collect(Collectors.toList())));
         return train;
     }
 
     public static void main(String[] args) throws Exception {
         if (args.length > 0) fontPatch = args[0];
         //it is best to use 14-16
-        int fontPixSize = args.length > 1 ? Integer.parseInt(args[1]) : size;
-        float fontSize = chooseSize(fontPixSize);
+        int fontPixSize = args.length > 1 ? Integer.parseInt(args[1]) : sizePixels;
+        Font mainFont = Font.createFont(Font.TRUETYPE_FONT, new File(fontPatch)).deriveFont((float) fontPixSize);
+        mainFont = chooseSize(mainFont, fontPixSize);
+        Font fallbackFont = new Font("Serif", Font.PLAIN, fontPixSize);
+        fallbackFont = chooseSize(fallbackFont, fontPixSize);
 
         System.out.println("Start loading OpenCV Java native library...");
         Loader.load(opencv_java.class);
@@ -133,18 +136,26 @@ public class NewSet {
         count = 0;
         for (var pair : train) {
             count++;
-            int size = pair.b.toCharArray().length, countFill = 0;
             // a fill can contain multiple characters
-            for (Character c : pair.b.toCharArray()) {
+            List<Integer> fillChars = pair.b;
+            int size = fillChars.size(), countFill = 0;
+            for (int codePoint : fillChars) {
                 countFill++;
+                Font font;
+                if (mainFont.canDisplay(codePoint)) {
+                    font = mainFont;
+                }
+                else if (fallbackFont.canDisplay(codePoint)) {
+                    font = fallbackFont;
+                }
+                else {
+                    throw new FontFormatException("symbol " + codePoint + " cannot be displayed");
+                }
                 BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
                 Graphics2D g2d = img.createGraphics();
-                Font font = Font.createFont(Font.TRUETYPE_FONT,
-                        new File(fontPatch)).deriveFont(fontSize);
-                // new Font("Arial", Font.PLAIN, 15);
                 g2d.setFont(font);
                 FontMetrics fm = g2d.getFontMetrics();
-                int width = fm.charWidth(c);
+                int width = fm.charWidth(codePoint);
                 int height = fm.getHeight();
                 g2d.dispose();
 
@@ -152,29 +163,21 @@ public class NewSet {
                 g2d = img.createGraphics();
                 g2d.setColor(Color.WHITE);
                 g2d.fillRect(0, 0, width, height);
-                g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
-                        RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                        RenderingHints.VALUE_ANTIALIAS_ON);
-                g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING,
-                        RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-                g2d.setRenderingHint(RenderingHints.KEY_DITHERING,
-                        RenderingHints.VALUE_DITHER_ENABLE);
-                g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,
-                        RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-                g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                        RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-                g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
-                        RenderingHints.VALUE_RENDER_QUALITY);
-                g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
-                        RenderingHints.VALUE_STROKE_PURE);
+                g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+                g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+                g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+                g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
                 g2d.setFont(font);
                 fm = g2d.getFontMetrics();
                 g2d.setColor(Color.BLACK);
-                g2d.drawString(String.valueOf(c), 0, fm.getAscent());
+                g2d.drawString(String.valueOf(Character.toChars(codePoint)), 0, fm.getAscent());
                 g2d.dispose();
 
-                if (c != ' ') {
+                if (!Character.isSpaceChar(codePoint)) {
                     // skip the space bar and normalize the images
                     OpenCVFrameConverter.ToOrgOpenCvCoreMat converter = new OpenCVFrameConverter.ToOrgOpenCvCoreMat();
                     Java2DFrameConverter java2dFrameConverter = new Java2DFrameConverter();
@@ -186,11 +189,9 @@ public class NewSet {
                     img = java2dFrameConverter.getBufferedImage(converter.convert(normalizeImg));
                 }
                 if (size == 1)
-                    ImageIO.write(img, "png", new File(String.
-                            format(outPatch + "%03d%s.png", count, pair.a)));
+                    ImageIO.write(img, "png", new File(String.format(outPatch + "%03d%s.png", count, pair.a)));
                 else
-                    ImageIO.write(img, "png", new File(String.
-                            format(outPatch + "%03d%s_%02d.png", count, pair.a, countFill)));
+                    ImageIO.write(img, "png", new File(String.format(outPatch + "%03d%s_%02d.png", count, pair.a, countFill)));
             }
         }
         System.out.println("symbols created, folder: " + outPatch.substring(outPatch
